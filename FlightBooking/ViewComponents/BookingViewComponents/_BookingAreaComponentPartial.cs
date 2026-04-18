f75c7ee7407ff7fbede7eb932d6c20fe956e534c
zaruyashar <203256006+zaruyashar@users.noreply.github.com>
2026-04-18 23:32:08 +0300

    tut 8 inc

---
+++ FlightBooking/ViewComponents/BookingViewComponents/_BookingAreaComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.BookingViewComponents
+{
+    public class _BookingAreaComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ FlightBooking/ViewComponents/BookingViewComponents/_BookingTagsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.BookingViewComponents
+{
+    public class _BookingTagsComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
