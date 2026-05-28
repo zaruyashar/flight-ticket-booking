7c8449c87e1f65cbfeea4dd3b6b526027a54b19d
zaruyashar <203256006+zaruyashar@users.noreply.github.com>
2026-05-28 21:34:20 +0300

    tutorial 10 completed

---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultDestinationOfferComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultDestinationOfferComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ FlightBooking/ViewComponents/DefaultViewComponents/_DefaultFlightStatusComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.ViewComponents.DefaultViewComponents
+{
+    public class _DefaultFlightStatusComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
